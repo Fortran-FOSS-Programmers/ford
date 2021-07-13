5d114f6022b7622372f2579ac9ff2e803a372265
Peter Hill <zed.three@gmail.com>
2021-07-13 16:28:01 +0100

    Apply black formatting to test

--- test/reader_test.py
+++ test/reader_test.py
@@ -10,31 +10,39 @@ import re
 
 import ford.reader as reader
 
-RE_WHITE=re.compile(r'\s+')
+RE_WHITE = re.compile(r"\s+")
+
 
 def remove_multiple_white_space(lines):
-    return [RE_WHITE.sub(' ',line.strip()) for line in lines]
+    return [RE_WHITE.sub(" ", line.strip()) for line in lines]
 
 
 def test_reader_test_data():
-    f_files = glob.glob('./test_data/*.f*')
-    f_files = [f for f in f_files 
-                   if  not 'expected' in f # remove 'expected' files
-                   and not 'bad' in f]     # remove 'bad files' 
-    #create_expected=True
-    create_expected=False
+    f_files = glob.glob("./test_data/*.f*")
+    f_files = [
+        f
+        for f in f_files
+        if "expected" not in f and "bad" not in f  # remove 'expected' files
+    ]  # remove 'bad files'
+    # create_expected=True
+    create_expected = False
     for ff in f_files:
-        ee=ff.replace('.f90','_expected.f90')
-        print('\tProcessing: %s \tExpected: %s '%(ff,ee))
-        lines=[line for line in reader.FortranReader(ff,docmark='!',predocmark='>',docmark_alt='#',predocmark_alt='<') ]
+        ee = ff.replace(".f90", "_expected.f90")
+        print("\tProcessing: %s \tExpected: %s " % (ff, ee))
+        lines = [
+            line
+            for line in reader.FortranReader(
+                ff, docmark="!", predocmark=">", docmark_alt="#", predocmark_alt="<"
+            )
+        ]
         if create_expected:
-            print('WARNING : Writing expected file '+ee)
-            with open(ee,'w') as ef:                    
+            print("WARNING : Writing expected file " + ee)
+            with open(ee, "w") as ef:
                 for line in lines:
-                    print(line) 
-                    ef.write(line+'\n')
+                    print(line)
+                    ef.write(line + "\n")
         else:
             with open(ee) as ef:
-                lines=remove_multiple_white_space(lines)
-                elines=remove_multiple_white_space(ef.readlines())
+                lines = remove_multiple_white_space(lines)
+                elines = remove_multiple_white_space(ef.readlines())
                 assert lines == elines
