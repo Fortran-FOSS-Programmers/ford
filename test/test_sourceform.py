b9e2af52d901faaf8bb19ae942ae7f936a75d52a
Peter Hill <zed.three@gmail.com>
2021-07-14 10:17:31 +0100

    Add test for contains in submodule procedures
    Checks that #321 works

--- test/test_sourceform.py
+++ test/test_sourceform.py
@@ -39,3 +39,39 @@ def test_extends(tmp_path):
     assert len(program.types) == 3
     assert program.types[1].extends == "base"
     assert program.types[2].extends == "base"
+
+
+def test_submodule_procedure_contains(tmp_path):
+    """Check that submodule procedures can have 'contains' statements"""
+
+    data = """\
+    module foo_m
+      implicit none
+      interface
+        module subroutine foo()
+          implicit none
+        end subroutine
+      end interface
+    end module
+
+    submodule(foo_m) foo_s
+      implicit none
+    contains
+      module procedure foo
+      contains
+        subroutine bar()
+        end subroutine
+      end procedure
+    end submodule
+    """
+
+    filename = tmp_path / "test.f90"
+    with open(filename, "w") as f:
+        f.write(data)
+
+    settings = defaultdict(str)
+    settings["docmark"] = "!"
+
+    fortran_type = FortranSourceFile(str(filename), settings)
+
+    assert len(fortran_type.modules) == 1
