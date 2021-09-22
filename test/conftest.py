78deeb2b8e9cc1a2f580f5b5978a2303a7dca842
Peter Hill <zed.three@gmail.com>
2021-09-22 15:56:57 +0100

    Reduce duplication in test fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -1,11 +1,34 @@
+import copy
+import ford
+import pathlib
 import pytest
+from textwrap import dedent
+
+# Ford default src folder
+DEFAULT_SRC = "src"
+
+
+def copy_file(data: str, path: pathlib.Path, filename: str) -> pathlib.Path:
+    """Write data to 'path/filename'"""
+    path.mkdir(exist_ok=True)
+    full_filename = path / filename
+    with open(full_filename, "w") as f:
+        f.write(dedent(data))
+    return full_filename
 
 
 @pytest.fixture
 def copy_fortran_file(tmp_path):
-    def copy_file(data):
-        filename = tmp_path / "test.f90"
-        with open(filename, "w") as f:
-            f.write(data)
-        return filename
-    return copy_file
+    return lambda data: copy_file(data, tmp_path / "src", "test.f90")
+
+
+@pytest.fixture
+def copy_settings_file(tmp_path):
+    return lambda data: copy_file(data, tmp_path, "test.md")
+
+
+@pytest.fixture
+def restore_macros():
+    old_macros = copy.copy(ford.utils._MACRO_DICT)
+    yield
+    ford.utils._MACRO_DICT = copy.copy(old_macros)
--- test/test_md_inputs.py
+++ test/test_md_inputs.py
@@ -1,65 +1,44 @@
 import sys
-import pytest
-from collections import defaultdict
 import ford
 import ford.fortran_project
 
 # Ford default src folder
 DEFAULT_SRC = "src"
 
-@pytest.fixture
-def copy_fortran_file(tmp_path):
-    def copy_file(fortran_data):
-        src_dir = tmp_path / DEFAULT_SRC
-        src_dir.mkdir()
-        filename = src_dir / "test.f90"
-        with open(filename, "w") as f:
-            f.write(fortran_data)
-        return src_dir
-    return copy_file
 
-@pytest.fixture
-def copy_settings_file(tmp_path):
-    def copy_settings(data):
-        md_file = tmp_path / "ford.md"
-        with open(md_file, "w") as f:
-            f.write(data)
-        return md_file
-    return copy_settings
-
-
-
-import warnings
-
-def test_extra_mods_empty(copy_fortran_file, copy_settings_file, monkeypatch, tmp_path):
+def test_extra_mods_empty(
+    copy_fortran_file, copy_settings_file, monkeypatch, restore_macros
+):
     """This checks that extra_mods is parsed correctly in input md file"""
 
     data = """\
-module test
-end module test
-"""
+    module test
+    end module test
+    """
     settings = """\
-extra_mods:
-"""
+    extra_mods:
+    """
 
     copy_fortran_file(data)
     md_file = copy_settings_file(settings)
 
     # Modify command line args with argv to use
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', ["ford", str(md_file)])
+        m.setattr(sys, "argv", ["ford", str(md_file)])
         ford.run()
 
 
-def test_extra_mods_intrinsic(copy_fortran_file, copy_settings_file, monkeypatch, tmp_path):
+def test_extra_mods_intrinsic(
+    copy_fortran_file, copy_settings_file, monkeypatch, restore_macros
+):
     """This checks that adding extra_mods doesn't change the module variable INTRINSIC_MODS"""
     data = """\
-module test
-end module test
-"""
+    module test
+    end module test
+    """
     settings = """\
-extra_mods: dummy: dummy_module
-"""
+    extra_mods: dummy: dummy_module
+    """
     # Initial value of intrinsic mods
     old_intrinsic_mods = ford.fortran_project.INTRINSIC_MODS.copy()
 
@@ -69,7 +48,7 @@ extra_mods: dummy: dummy_module
 
     # Modify command line args with argv to use
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', ["ford", str(md_file)])
+        m.setattr(sys, "argv", ["ford", str(md_file)])
         ford.run()
 
     # Check that the module level variable has not been changed
