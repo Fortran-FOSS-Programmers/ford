94af078747945e39afbbc4c3cb9829cb8d3df286
Peter Hill <peter.hill@york.ac.uk>
2023-08-22 11:14:00 +0100

    Add WIP tree-sitter cursor-based parser

--- ford/tree_sitter_parser.py
+++ ford/tree_sitter_parser.py
@@ -13,10 +13,11 @@ from ford.sourceform import (
     _can_have_contains,
 )
 from typing import List, Optional
+from contextlib import contextmanager
 
 try:
     import tree_sitter_languages
-    from tree_sitter import Node, Language
+    from tree_sitter import Node, Language, TreeCursor
 
     FREE_FORM_LANGUAGE = tree_sitter_languages.get_language("fortran")
     FIXED_FORM_LANGUAGE = tree_sitter_languages.get_language("fixed_form_fortran")
@@ -280,6 +281,317 @@ class TreeSitterParser:
         return FortranNamelist(
             node, self, parent=parent, name=self._get_name(node), vars=variables
         )
+
+    def parse_derived_type(self, parent, node):
+        pass
+
+    def parse_interface(self, parent, node):
+        pass
+
+    def parse_bound_procedure(self, parent, node):
+        pass
+
+    def parse_final_procedure(self, parent, node):
+        pass
+
+    def parse_enum(self, parent, node):
+        pass
+
+
+@contextmanager
+def descend_one_node(cursor: TreeCursor) -> TreeCursor:
+    cursor.goto_first_child()
+    try:
+        yield cursor
+    finally:
+        cursor.goto_parent()
+
+
+class TreeSitterCursorParser:
+    def __init__(self, free_form: bool = True, encoding: str = "utf-8"):
+        self.encoding = encoding
+
+        self.language = FREE_FORM_LANGUAGE if free_form else FIXED_FORM_LANGUAGE
+        self.parser = FREE_FORM_PARSER if free_form else FIXED_FORM_PARSER
+
+        self.name_query = Query(self.language, "(name) @name")
+
+    def _get_name(self, node: TreeCursor):
+        name_capture = self.name_query(node.node)[0]
+        return name_capture.text.decode(self.encoding)
+
+    @staticmethod
+    def read_docstring(cursor: TreeCursor, docmark: str) -> List[str]:
+        comments = []
+
+        node = cursor.node
+
+        prev_sibling = node if node.prev_sibling else node.parent
+        while prev_sibling := prev_sibling.prev_sibling:
+            if prev_sibling.type != "comment":
+                break
+            comment = prev_sibling.text.decode()
+            if is_predoc_comment(comment, "!>", "!*"):
+                comments.append(remove_prefixes(comment, "!>", "!*"))
+
+        next_sibling = node
+        while next_sibling := next_sibling.next_sibling:
+            if next_sibling.type != "comment":
+                break
+            comment = next_sibling.text.decode()
+            if is_doc_comment(comment, "!!", "!<"):
+                comments.append(remove_prefixes(comment, "!!", "!<"))
+
+        return comments
+
+    def warn_invalid_node(self, entity: FortranContainer, collection: str):
+        print(f"{entity.obj} {entity.name!r} can't have {collection}")
+
+    def parse_source(self, entity: FortranContainer, cursor: TreeCursor):
+        if cursor.node.type == "translation_unit":
+            cursor.goto_first_child()
+
+        while cursor.goto_next_sibling():
+            # print(cursor.node.type, cursor.node.start_point)
+            if cursor.node.type == "program":
+                if not hasattr(entity, "programs"):
+                    self.warn_invalid_node(entity, "programs")
+                    continue
+
+                with descend_one_node(cursor) as cursor:
+                    entity.programs.append(self.parse_program(entity, cursor))
+
+            elif cursor.node.type == "module":
+                if not hasattr(entity, "modules"):
+                    self.warn_invalid_node(entity, "modules")
+                    continue
+
+                with descend_one_node(cursor) as cursor:
+                    entity.modules.append(self.parse_module(entity, cursor))
+
+            elif cursor.node.type == "submodule":
+                if not hasattr(entity, "submodules"):
+                    self.warn_invalid_node(entity, "submodules")
+                    continue
+
+                with descend_one_node(cursor) as cursor:
+                    entity.submodules.append(self.parse_submodule(entity, cursor))
+
+            elif cursor.node.type == "internal_procedures":
+                if not isinstance(entity, _can_have_contains):
+                    print(f"{entity.obj} {entity.name!r} can't have functions and that")
+
+                with descend_one_node(cursor) as cursor:
+                    self.parse_source(entity, cursor)
+
+            elif cursor.node.type == "function":
+                if not hasattr(entity, "functions"):
+                    self.warn_invalid_node(entity, "functions")
+                    continue
+
+                with descend_one_node(cursor) as cursor:
+                    entity.functions.append(self.parse_function(entity, cursor))
+
+            elif cursor.node.type == "subroutine":
+                if not hasattr(entity, "subroutines"):
+                    self.warn_invalid_node(entity, "subroutines")
+                    continue
+
+                with descend_one_node(cursor) as cursor:
+                    entity.subroutines.append(self.parse_subroutine(entity, cursor))
+
+            elif cursor.node.type == "variable_declaration":
+                if not hasattr(entity, "variables"):
+                    self.warn_invalid_node(entity, "variables")
+                    continue
+
+                with descend_one_node(cursor):
+                    entity.variables.extend(self.parse_variable(entity, cursor))
+
+            elif cursor.node.type == "namelist_statement":
+                if not hasattr(entity, "namelists"):
+                    self.warn_invalid_node(entity, "namelists")
+                    continue
+
+                with descend_one_node(cursor):
+                    entity.namelists.extend(self.parse_namelist(entity, cursor))
+
+        # attributes
+
+        # block data
+
+        # associate
+
+        # namelists
+
+        # derived type
+
+        # interface
+
+        # boundproc
+
+        # finalproc
+
+        # enum
+
+        # common
+
+        # variable
+
+        # use
+
+        # calls
+
+        # if not isinstance(entity, _can_have_contains):
+        #     return
+
+        # if not (contains := self.contains_query.maybe_first(cursor)):
+        #     return
+
+        # if isinstance(entity, FortranType):
+        #     self.parse_derived_type(entity, cursor)
+        #     return
+
+    def parse_program(
+        self, parent: FortranContainer, program: TreeCursor
+    ) -> FortranProgram:
+        return FortranProgram(self, program, name=self._get_name(program))
+
+    def parse_module(
+        self, parent: FortranContainer, module: TreeCursor
+    ) -> FortranModule:
+        return FortranModule(
+            self,
+            module,
+            parent=parent,
+            name=self._get_name(module),
+        )
+
+    def parse_submodule(
+        self, parent: FortranContainer, submodule: TreeCursor
+    ) -> FortranSubmodule:
+        return FortranSubmodule(self, submodule, name=self._get_name(submodule))
+
+    def parse_attributes(self, parent, attributes):
+        pass
+
+    def parse_block_data(self, parent, node):
+        pass
+
+    def parse_associate(self, parent, node):
+        pass
+
+    def parse_subroutine(self, parent, node):
+        # attributes = self.function_attributes_query(node)
+        # arguments = self.procedure_parameters_query.first(node)
+        # bind = self.language_binding_query.maybe_first(node)
+
+        subroutine = FortranSubroutine(
+            self,
+            node,
+            parent=parent,
+            name=self._get_name(node),
+            attributes=[],
+            arguments=[],
+            bindC=None,
+        )
+        subroutine._cleanup()
+        return subroutine
+
+    def parse_function(
+        self, parent: FortranContainer, cursor: TreeCursor
+    ) -> FortranFunction:
+        attributes = []
+        result = None
+        result_type = None
+        arguments = ""
+        bindC = None
+
+        with descend_one_node(cursor):
+            while True:
+                if cursor.node.type == "procedure_qualifier":
+                    attributes.append(decode(cursor.node))
+                elif cursor.node.type in ("intrinsic_type", "derived_type"):
+                    result_type = decode(cursor.node)
+                elif cursor.node.type == "parameters":
+                    arguments = decode(cursor.node)
+                elif cursor.node.type == "function_result":
+                    result = decode(cursor.node.named_children[0])
+                elif cursor.node.type == "language_binding":
+                    bindC = decode(cursor.node)
+
+                if not cursor.goto_next_sibling():
+                    break
+
+        function = FortranFunction(
+            self,
+            cursor,
+            parent=parent,
+            name=self._get_name(cursor),
+            attributes=",".join(attributes),
+            result=result,
+            arguments=arguments,
+            bindC=bindC,
+        )
+
+        if result_type is not None:
+            function.retvar = FortranVariable(
+                name=function.retvar, parent=function, vartype=result_type
+            )
+
+        function._cleanup()
+        return function
+
+    def parse_variable(
+        self, parent: FortranContainer, cursor: TreeCursor
+    ) -> List[FortranVariable]:
+        attributes = []
+        vartype = None
+        names = []
+
+        while True:
+            if cursor.node.type in ["intrinsic_type", "derived_type"]:
+                vartype = decode(cursor.node)
+            elif cursor.node.type == "type_qualifier":
+                attributes.append(decode(cursor.node))
+            elif cursor.node.type == "identifier":
+                names.append(decode(cursor.node))
+
+            if not cursor.goto_next_sibling():
+                break
+
+        return [
+            FortranVariable(
+                name=name, parent=parent, vartype=vartype, attribs=attributes
+            )
+            for name in names
+        ]
+
+    def parse_namelist(
+        self, parent: FortranContainer, cursor: TreeCursor
+    ) -> List[FortranNamelist]:
+        namelists = []
+
+        while cursor.goto_next_sibling():
+            if cursor.node.type != "variable_group":
+                continue
+            with descend_one_node(cursor):
+                name = None
+                variables = []
+                while True:
+                    if cursor.node.type == "name":
+                        name = decode(cursor.node)
+                    elif cursor.node.type == "identifier":
+                        variables.append(decode(cursor.node))
+                    if not cursor.goto_next_sibling():
+                        break
+                namelists.append(
+                    FortranNamelist(
+                        cursor, self, parent=parent, name=name, vars=variables
+                    )
+                )
+
+        return namelists
 
     def parse_derived_type(self, parent, node):
         pass
--- test/test_tree_sitter_parser.py
+++ test/test_tree_sitter_parser.py
@@ -1,52 +1,61 @@
-from ford.tree_sitter_parser import tree_sitter_parser_available, TreeSitterParser
+from ford.tree_sitter_parser import (
+    tree_sitter_parser_available,
+    TreeSitterParser,
+    TreeSitterCursorParser,
+)
 from ford.sourceform import FortranSourceFile
 from ford.settings import ProjectSettings
 
 from textwrap import dedent
 
+import time
 import pytest
+
+
+data = dedent(
+    """
+!> predoc
+module foo
+!! post doc
+  !> type predoc
+  type :: base
+      !! type post doc
+      !> variable predoc
+      integer :: bar
+      !! variable postdoc
+  end type
+contains
+    pure integer function bar(x, a, b) result(y)
+      integer, intent(in) :: x, a, b
+      real :: zing, zang, zong
+      namelist /ben/ zing, zang, zong
+
+      contains
+        pure function ziff(z)
+          real :: ziff, z
+          complex:: fa, ba, ga
+          namelist /zaff/ fa, ba, ga
+        end function ziff
+    end function bar
+end module foo
+"""
+).encode()
 
 
 @pytest.mark.skipif(not tree_sitter_parser_available, reason="Requires tree-sitter")
 def test_tree_sitter_parser():
-    data = dedent(
-        """
-    !> predoc
-    module foo
-    !! post doc
-      !> type predoc
-      type :: base
-          !! type post doc
-          !> variable predoc
-          integer :: bar
-          !! variable postdoc
-      end type
-    contains
-        pure integer function bar(x, a, b) result(y)
-          integer, intent(in) :: x, a, b
-          real :: zing, zang, zong
-          namelist /ben/ zing, zang, zong
-
-          contains
-            pure function ziff(z)
-              real :: ziff, z
-              complex:: fa, ba, ga
-              namelist /zaff/ fa, ba, ga
-            end function ziff
-        end function bar
-    end module foo
-    """
-    ).encode()
-
     parser = TreeSitterParser()
     tree = parser.parser.parse(data)
 
+    time_start = time.time()
     fortran_file = FortranSourceFile(
         "imaginary.f90",
         ProjectSettings(),
         parser,
         source=tree.root_node,
     )
+    time_end = time.time()
+    print(f"Query based: {time_end - time_start}")
     assert len(fortran_file.modules) == 1
     assert fortran_file.modules[0].name == "foo"
 
@@ -64,3 +73,41 @@ def test_tree_sitter_parser():
     assert function.namelists[0].name == "ben"
 
     assert len(function.functions) == 1
+
+
+@pytest.mark.skipif(not tree_sitter_parser_available, reason="Requires tree-sitter")
+def test_tree_sitter_cursor_parser():
+    parser = TreeSitterCursorParser()
+    tree = parser.parser.parse(data)
+
+    time_start = time.time()
+    fortran_file = FortranSourceFile(
+        "imaginary.f90",
+        ProjectSettings(),
+        parser,
+        source=tree.walk(),
+    )
+    time_end = time.time()
+    print(f"Cursor based: {time_end - time_start}")
+    assert len(fortran_file.modules) == 1
+    assert fortran_file.modules[0].name == "foo"
+
+    functions = fortran_file.modules[0].functions
+    assert len(functions) == 1
+    function = functions[0]
+    assert function.name == "bar"
+    retvar = function.retvar
+    assert retvar.name == "y"
+    assert retvar.full_type == "integer"
+    arg_names = sorted(arg.name for arg in function.args)
+    assert arg_names == sorted(["x", "a", "b"])
+
+    assert len(function.namelists) == 1
+    assert function.namelists[0].name == "ben"
+
+    assert len(function.functions) == 1
+
+
+if __name__ == "__main__":
+    test_tree_sitter_parser()
+    test_tree_sitter_cursor_parser()
