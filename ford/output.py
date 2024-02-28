cf3fb8198f55c9dcc9c2f921a7730f1ea4e97365
Peter Hill <peter.hill@york.ac.uk>
2024-02-28 09:55:56 +0000

    Remove unused `show_proc_parent` option
    Closes #630

--- ford/output.py
+++ ford/output.py
@@ -201,7 +201,6 @@ class Documentation:
             self.data.get("graph_dir", ""),
             graphparent,
             settings.coloured_edges,
-            settings.show_proc_parent,
             save_graphs=bool(self.data.get("graph_dir", False)),
         )
 
