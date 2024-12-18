5f42ba8fc8ed5abf9aee200f0d8865280117888a
Peter Hill <peter.hill@york.ac.uk>
2024-12-18 15:21:27 +0000

    Remove query-based parser

--- test/test_tree_sitter_parser.py
+++ test/test_tree_sitter_parser.py
@@ -1,4 +1,4 @@
-from ford.tree_sitter_parser import TreeSitterParser, TreeSitterCursorParser
+from ford.tree_sitter_parser import TreeSitterParser
 from ford.sourceform import FortranSourceFile
 from ford.settings import ProjectSettings
 
@@ -37,40 +37,8 @@ end module foo
 ).encode()
 
 
-def test_tree_sitter_parser():
-    parser = TreeSitterParser()
-    tree = parser.parser.parse(data)
-
-    time_start = time.time()
-    fortran_file = FortranSourceFile(
-        "imaginary.f90",
-        ProjectSettings(),
-        parser,
-        source=tree.root_node,
-    )
-    time_end = time.time()
-    print(f"Query based: {time_end - time_start}")
-    assert len(fortran_file.modules) == 1
-    assert fortran_file.modules[0].name == "foo"
-
-    functions = fortran_file.modules[0].functions
-    assert len(functions) == 1
-    function = functions[0]
-    assert function.name == "bar"
-    retvar = function.retvar
-    assert retvar.name == "y"
-    assert retvar.full_type == "integer"
-    arg_names = sorted(arg.name for arg in function.args)
-    assert arg_names == sorted(["x", "a", "b"])
-
-    assert len(function.namelists) == 1
-    assert function.namelists[0].name == "ben"
-
-    assert len(function.functions) == 1
-
-
 def test_tree_sitter_cursor_parser():
-    parser = TreeSitterCursorParser()
+    parser = TreeSitterParser()
     tree = parser.parser.parse(data)
 
     time_start = time.time()
