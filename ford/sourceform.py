d89622fd8824ccba3312f45f17fa38ad2931a4c1
Kuan-Chih Wang <kuanchihw@ucar.edu>
2025-01-17 19:58:23 -0700

    Fix incorrect handling for the `summary` key in documentation metadata

--- ford/sourceform.py
+++ ford/sourceform.py
@@ -432,7 +432,7 @@ class FortranBase:
             )
 
         if self.meta.summary is not None:
-            self.meta.summary = md.convert("\n".join(self.meta.summary), context=self)
+            self.meta.summary = md.convert(" ".join(self.meta.summary.split()), context=self)
         elif paragraph := PARA_CAPTURE_RE.search(self.doc):
             # If there is no stand-alone webpage for this item, e.g.
             # an internal routine, make the whole doc blob appear,
